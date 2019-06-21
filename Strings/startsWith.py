b15808ca3eeeef3564c06df38582ba29033354d1
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-06-21 03:17:04 -0500

    w3 solutions

---
+++ Strings/startsWith.py
@@ -0,0 +1,10 @@
+#Write a Python program to check whether a string starts with specified characters.
+
+
+def main():
+    sample_string = 'w3resource'
+    start = 'w3'
+    print(sample_string.startswith('w3'))
+
+
+main()

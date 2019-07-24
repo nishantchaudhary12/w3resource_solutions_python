fb97b7550b7eada325a4aef0205608b5916505ce
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-07-23 21:39:31 -0500

    w3 solutions

---
+++ Tuple/fourthElement.py
@@ -0,0 +1,10 @@
+#Write a Python program to get the 4th element and 4th element from last of a tuple.
+
+
+def main():
+    x = ('w', '3', 'r', 'e', 's', 'o', 'u', 'r', 'c', 'e')
+    print('4th element from start:', x[4])
+    print('4th element from end:', x[-4])
+
+
+main()

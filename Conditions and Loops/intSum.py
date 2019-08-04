9584c5ab034d30928813d411c4533e21f12062b6
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-08-04 18:23:50 -0500

    w3 solutions

---
+++ Conditions and Loops/intSum.py
@@ -0,0 +1,11 @@
+#Write a Python program to convert month name to a number of days.
+
+
+def sumIntegers(x, y):
+    result = x + y
+    if result in range(15, 20):
+        return 20
+    return result
+
+
+print(sumIntegers(10, 5))

d0713a3ab0e6879c795edfe459d63146373a9d23
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-08-15 15:59:21 -0500

    w3 solutions

---
+++ Dictionary/dictToTable.py
@@ -0,0 +1,9 @@
+#Write a Python program to print a dictionary in table format.
+
+
+my_dict = {'C1': [1, 2, 3], 'C2': [5, 6, 7], 'C3': [9, 10, 11]}
+
+result = sorted(my_dict.items())
+
+for row in zip(*([key] + (value) for key, value in result)):
+    print(*row)

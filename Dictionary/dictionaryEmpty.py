9e383abb2c8d78bd9c02694c4ac88f0b899c37db
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-08-19 23:57:15 -0500

    w3 resources

---
+++ Dictionary/dictionaryEmpty.py
@@ -0,0 +1,9 @@
+#Write a Python program to check a dictionary is empty or not.
+
+
+my_dict = dict()
+
+if not my_dict:
+    print("Dictionary is Empty")
+else:
+    print("Dictionary is not Empty")
